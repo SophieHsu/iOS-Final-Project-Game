05030751675c2464ef5d58377f7c4714827c9947
Sophie Hsu <fidlar@ip210-206.wlan.ntust.edu.tw>
2017-06-12 01:55:46 +0800

    add bg and brick

--- iOS Final Project Game/GameViewController.swift
+++ iOS Final Project Game/GameViewController.swift
@@ -20,6 +20,8 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
     var geometry:SCNGeometry!
     var geometryNode:SCNNode!
     
+//  Chess piece
+    
 //  SKScene Nodes
     let up = SKSpriteNode(imageNamed:"up")
     let down = SKSpriteNode(imageNamed:"down")
@@ -28,8 +30,14 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
     let downRight = SKSpriteNode(imageNamed:"downRight")
     let downLeft = SKSpriteNode(imageNamed:"downLeft")
     let ok = SKSpriteNode(imageNamed:"ok")
+    let turn = SKSpriteNode(imageNamed:"turn")
 //  Variables
     var TouchState = ""
+    var brickNum = 2
+    var turnState = 1
+    
+//  Array for brick height
+    var brickHArray = Array(repeating: Array(repeating: 0, count: 10), count: 10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +46,8 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
         setupScene()
         setupSKScene()
         setupCamera()
-        spawnShape()
+        setupPieces()
+        spawnShape(length: 1)
         print(scnView.isUserInteractionEnabled)
         print(skScene.isUserInteractionEnabled)
         let tapGesture = UITapGestureRecognizer(target: self, action:
@@ -72,6 +81,9 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
         } else if self.ok.contains(location) {
             TouchState = "ok"
             print("ok!")
+        } else if self.turn.contains(location) {
+            TouchState = "turn"
+            print("turn!")
         }
         moveBrick(touch: TouchState)
         //print(geometryNode.position)
@@ -98,10 +110,12 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
         scnView.scene = scnScene
         var geometry:SCNGeometry
         geometry = SCNBox(width: 100.0, height: 0.0, length: 100.0, chamferRadius: 0.0)
-        geometry.firstMaterial?.diffuse.contents = UIColor.green
+        geometry.firstMaterial?.diffuse.contents = UIImage.init(named: "base.jpg")
         let geometryNode = SCNNode(geometry: geometry)
         scnScene.rootNode.addChildNode(geometryNode)
-        geometryNode.position = SCNVector3(x: 0, y: -30, z: 0)
+        geometryNode.position = SCNVector3(x: 0, y: 0, z: 0)
+        geometryNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        geometryNode.physicsBody?.isAffectedByGravity = false
     }
     
     func setupSKScene(){
@@ -115,7 +129,42 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
         skScene.addChild(downRight)
         skScene.addChild(downLeft)
         skScene.addChild(ok)
+        skScene.addChild(turn)
         scnView.overlaySKScene = skScene
+    }
+    
+//    func collada2SCNNode(filepath:String) -> SCNNode {
+//        var node = SCNNode()
+//        let scene = SCNScene(named: filepath)
+////        var nodeArray = scene!.rootNode.childNodes
+//        
+////        for childNode in nodeArray {
+//            node.addChildNode(childNode as SCNNode)
+////        }
+//        return node
+//    }
+    
+    func setupPieces(){
+        geometry = SCNCone(topRadius: 0.0, bottomRadius: 3.5, height: 7.0)
+        geometry.firstMaterial?.diffuse.contents = UIColor.brown
+        geometryNode = SCNNode(geometry: geometry)
+        scnScene.rootNode.addChildNode(geometryNode)
+        geometryNode.position = SCNVector3(x: 3.6+14.0, y: 3.5, z: 3.6+14.0)
+        geometryNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        geometryNode.physicsBody?.isAffectedByGravity = true
+
+        
+        // Load the character.
+//        let characterScene = SCNScene.init(url: "Assets.xcassets/queen.dae", options: [:])
+//        let characterTopLevelNode = characterScene.RootNode.ChildNodes[0];
+//        
+//        scnScene.rootNode.addChildNode(characterTopLevelNode)
+////        let myNode = collada2SCNNode(filepath: "Assets.xcassets/queen.dataset/queen.dae")
+////        print(myNode)
+//        let assetScene = SCNScene(named: "queen.dae")
+////        let node = assetScene?.rootNode.childNode[0]
+//        scnScene.rootNode.addChildNode(node)
+        
     }
     
     func setupControlBtn(){
@@ -140,28 +189,30 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
         ok.size = CGSize(width: 60, height: 50)
         ok.position = CGPoint(x: 150, y: 80)
 
+        turn.size = CGSize(width: 60, height: 50)
+        turn.position = CGPoint(x: 220, y: 80)
     }
     
     func setupCamera() {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 100)
+        cameraNode.position = SCNVector3(x: 0, y: 80, z: 100)
         cameraNode.camera?.xFov = 60
         cameraNode.camera?.yFov  = 60
         cameraNode.camera?.zFar = 1000
         cameraNode.camera?.zNear = 0.01
-        cameraNode.rotation = SCNVector4(0.0, 0.0, 0.0, -Float(M_PI_4))
+        cameraNode.rotation = SCNVector4(1.0, 0.0, 0.0, -Float(M_PI_4)/2)
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
-    func spawnShape() {
-        geometry = SCNBox(width: 3.0, height: 3.0, length: 3.0, chamferRadius: 0.0)
+    func spawnShape(length: Double) {
+        geometry = SCNBox(width: 7.0, height: 7.0, length: CGFloat(7.0*length), chamferRadius: 0.0)
         geometry.firstMaterial?.diffuse.contents = UIColor.brown
         geometryNode = SCNNode(geometry: geometry)
         scnScene.rootNode.addChildNode(geometryNode)
-        geometryNode.position = SCNVector3(x: 0, y: 25, z: 0)
-        geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        geometryNode.physicsBody?.isAffectedByGravity = false
+        geometryNode.position = SCNVector3(x: 3.6, y: 73.5, z: 3.6)
+        geometryNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        geometryNode.physicsBody?.isAffectedByGravity = true
 //        geometryNode.rotation = SCNVector4Make(1.0, 0.0, 0.0, Float(M_PI_4))
 //        let force = SCNVector3(x: 1, y: 1 , z: 1)
 //        var position = SCNVector3(x: 1.0, y: 0.0, z: 0.0)
@@ -176,42 +227,117 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SKSceneDel
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         moveBrick(touch: TouchState)
-        
         print(geometryNode.position)
+        
     }
     
-    func moveBrick(touch: String){
+    func moveBrick(touch: String) {
         //check position
         if touch == "up" {
-            geometryNode.position.y += 3
+            geometryNode.position.y += 7
         } else if touch == "down" {
-            geometryNode.position.y -= 3
+            print(geometryNode.boundingBox.min)
+            if(geometryNode.position.y > 3.5) {
+                geometryNode.position.y -= 7
+            }
         } else if touch == "upRight" {
-            geometryNode.position.x += 3
-            geometryNode.position.z -= 3
+            geometryNode.position.x += 7
+//            geometryNode.position.z -= 7
         } else if touch == "upLeft" {
-            geometryNode.position.x -= 3
-            geometryNode.position.z -= 3
+//            geometryNode.position.x -= 7
+            geometryNode.position.z -= 7
         } else if touch == "downRight" {
-            geometryNode.position.x += 3
-            geometryNode.position.z += 3
+//            geometryNode.position.x += 7
+            geometryNode.position.z += 7
         } else if touch == "downLeft" {
-            geometryNode.position.x -= 3
-            geometryNode.position.z += 3
+            geometryNode.position.x -= 7
+//            geometryNode.position.z += 7
         } else if touch == "ok" {
-//            for i in scnScene.rootNode.childNodes {
-            
-//            }
-            spawnShape()
+            if(brickNum > 0){
+                var i = Int(geometryNode.position.x)
+                var j = Int(geometryNode.position.z)
+                let length = Double(Int(arc4random_uniform(11) + 1))
+                print(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)
+                print(geometryNode.boundingBox.max.z - geometryNode.boundingBox.min.z)
+                if(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x) >= (geometryNode.boundingBox.max.z - geometryNode.boundingBox.min.z){
+                    print(Int(Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0))
+                    for k in (0 ... Int((Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0) - 1)/2) {
+                        print("k = ", k)
+                        print((i+35)/7-k, (j+35)/7, Int(geometryNode.boundingBox.max.y), separator:" ", terminator:"")
+                        brickHArray[(i+35)/7-k][(j+35)/7] = Int(geometryNode.boundingBox.max.y)
+                    }
+                    i = Int(geometryNode.position.x)
+                    j = Int(geometryNode.position.z)
+                    print(Int(Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0))
+                    for k in (0 ... Int((Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0) - 1)/2) {
+                        print("k = ", k)
+                        print((i+35)/7+k, (j+35)/7, Int(geometryNode.boundingBox.max.y), separator:" ", terminator:"")
+                        brickHArray[(i+35)/7+k][(j+35)/7] = Int(geometryNode.boundingBox.max.y)
+                    }
+                }else{
+                    print(Int(Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0))
+                    for k in (0 ... Int((Double(geometryNode.boundingBox.max.z - geometryNode.boundingBox.min.z)/7.0) - 1)/2) {
+                        print("k = ", k)
+                        print((i+35)/7, (j+35)/7-k, Int(geometryNode.boundingBox.max.y), separator:" ", terminator:"")
+                        brickHArray[(i+35)/7][(j+35)/7-k] = Int(geometryNode.boundingBox.max.y)
+                    }
+                    i = Int(geometryNode.position.x)
+                    j = Int(geometryNode.position.z)
+                    print(Int(Double(geometryNode.boundingBox.max.x - geometryNode.boundingBox.min.x)/7.0))
+                    for k in (0 ... Int((Double(geometryNode.boundingBox.max.z - geometryNode.boundingBox.min.z)/7.0) - 1)/2) {
+                        print("k = ", k)
+                        print((i+35)/7, (j+35)/7+k, Int(geometryNode.boundingBox.max.y), separator:" ", terminator:"")
+                        brickHArray[(i+35)/7][(j+35)/7+k] = Int(geometryNode.boundingBox.max.y)
+                    }
+                }
+                brickHArray[(i+35)/7][(j+35)/7] = Int(geometryNode.boundingBox.max.y)
+                spawnShape(length: length)
+                brickNum -= 1
+            }else{
+                for var j in 0...9{
+                    for var i in 0...9{
+                        print(brickHArray[i][j], terminator:"")
+                    }
+                    print()
+                }
+//                movePiece()
+            }
         } else if touch == "turn" {
+            if(turnState < 3){
+                turnState += 1
+            }else{
+                turnState = 1
+            }
+            switch(turnState){
+            case 1:
+                geometryNode.rotation = SCNVector4Make(1.0, 0.0, 0.0, Float(M_PI_2))
+                break
+            case 2:
+                geometryNode.rotation = SCNVector4Make(0.0, 1.0, 0.0, Float(M_PI_2))
+                break
+            case 3:
+                geometryNode.rotation = SCNVector4Make(0.0, 0.0, 1.0, Float(M_PI_2))
+                break
+            default:
+                break
+            }
             
         }
 //        let options = [SCNHitTestRootNodeKey: sceneView.scene!.rootNode, SCNHitTestClipToZRangeKey: 15, SCNHitTestSortResultsKey: true]
 //        let hits = sceneView.hitTest(point, options: options)
 //        print(hits.first?.worldCoordinates)
-        print(geometryNode.hitTestWithSegment(from: SCNVector3(x: geometryNode.position.x-1.5,y: geometryNode.position.y-1.5,z: 0), to: SCNVector3(x: geometryNode.position.x+2,y: geometryNode.position.y+2,z: 0), options: [:]))
-        //                    print("interset!")
+//        print(geometryNode.position)
+//        print(geometryNode.position.x-2)
+//        print(geometryNode.position.y-2)
+//        print(geometryNode.position.z-2)
+//        print(geometryNode.hitTestWithSegment(from: SCNVector3(x: geometryNode.position.x-2,y: geometryNode.position.y-2,z: geometryNode.position.z-2), to: SCNVector3(x: geometryNode.position.x+2,y: geometryNode.position.y+2,z: geometryNode.position.z+2), options: [:]).count)
+////        //                    print("interset!")
         TouchState = ""
+    }
+    
+    func viewDidAppear(){
+        print(geometryNode.hitTestWithSegment(from: SCNVector3(x: geometryNode.position.x-1,y: geometryNode.position.y-1,z: 0), to: SCNVector3(x: geometryNode.position.x+2,y: geometryNode.position.y+2,z: 0), options: [:]).count)
+        
     }
 
 }
